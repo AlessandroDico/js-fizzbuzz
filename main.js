581c409a96f93d4c53d98d159dde82ca99a9d670
Ale <alessandrochb@gmail.com>
2020-10-20 16:00:14 +0200

    inizio seconda soluzione per poter stilizzare con HTML e CSS

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ al posto dei multipli di 5, deve stampare "Buzz"
 al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
 */
 
+
+/*
+// ___________PRIMA SOLUZIONE______________
+
 // STAMPO I 100NUMERI
 for (var i = 1; i <= 100; i++) {
     // console.log(i);
@@ -18,3 +22,78 @@ for (var i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+//_______________FINE PRIMA SOLUZIONE____________
+*/
+
+
+//__________________SECONDA SOLUZIONE____________
+// SOLUZIONE PER CSS E HTML
+
+var final_container = [];
+
+
+for (var i = 1; i <= 100; i++) {
+    // console.log(i);
+    if (((i % 3) == 0) && ((i % 5) == 0)) {
+        console.log('FizzBuzz');
+        final_container.push('FizzBuzz');
+    } else if ((i % 3) == 0) {
+        console.log('Fizz');
+        final_container.push('Fizz');
+    } else if ((i % 5) == 0) {
+        console.log('Buzz');
+        final_container.push('Buzz');
+    } else {
+        console.log(i);
+        final_container.push(i);
+    }
+}
+
+for (var i = 0; i < final_container.length; i++) {
+    document.write("<p>" + final_container[i] + "</p>");
+    // document.getElementById('all_numbers').innerHTML = final_container[i];
+    // document.getElementById('all_numbers').innerHTML = ("<p>" + final_container[i] + "</p>");
+
+}
+// document.writeln(final_container);
+// console.log(final_container);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**/
