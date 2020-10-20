55ce1624b53431f9bf9cd77e462532979aa6d62c
Ale <alessandrochb@gmail.com>
2020-10-20 15:07:41 +0200

    esercizio concluso e stampato

--- main.js
+++ main.js
@@ -7,5 +7,14 @@ al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
 
 // STAMPO I 100NUMERI
 for (var i = 1; i <= 100; i++) {
-    console.log(i);
+    // console.log(i);
+    if (((i % 3) == 0) && ((i % 5) == 0)) {
+        console.log('FizzBuzz');
+    } else if ((i % 3) == 0) {
+        console.log('Fizz');
+    } else if ((i % 5) == 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
 }
